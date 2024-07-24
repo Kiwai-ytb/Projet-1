86f0cfd365435cb07ab768aa2c36c880cd45abc6
Kiwai-ytb <puma.sacha@gmail.com>
2024-07-24 02:46:29 +0200

    Some new changes

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-
-# Importe les différentes bibliotèques requises ou différents modules
-import defs
-from colorama import Back, Fore, Style, init, deinit
-
-
-# Initialise les composant de la blibliothèque colorama
-init(autoreset=True)
-
-
-# Créer un espace pour plus de visibilité
-print("\n")
+if __name__ == "main":
+    
+    # Importe les différentes bibliotèques requises ou différents modules
+    try:
+        import defs
+        from colorama import Back, Fore, Style, init, deinit
+        init(autoreset=True)
+    except:
+        print(f"\n[!] Error: Import of modules failed")
+        exit(1)
 
 
-# Permet au joueur de créer son personnage
-print(f"{Fore.MAGENTA + Style.NORMAL}Garde{Style.RESET_ALL} : Hop hop hop, arrêtez vous içi, avant d'aller plus loin, j'aurais besoin d'une piéce d'identité.\n\n")
-
-print("\033[4mPiéce d'identité\033[0m\n")
-nom_prenom = defs.demander_nom_prenom()
-age = defs.demander_age()
-nationalite = defs.demander_nationatlite()
-raison_venue = defs.demander_raison_venue(nom_prenom)
-print(f"{Fore.MAGENTA + Style.NORMAL}Garde{Style.RESET_ALL} : Tout me semble bon, vous pouvez passé.\n")
+    # Créer un espace pour plus de visibilité
+    print("\n")
 
 
-# Appelle la fonction créer dans le module defs appeller pensee_une
-pensee_une = defs.demander_pensee_une()
+    # Permet au joueur de créer son personnage
+    print(f"{Fore.MAGENTA + Style.NORMAL}Garde{Style.RESET_ALL} : Hop hop hop, arrêtez vous içi, avant d'aller plus loin, j'aurais besoin d'une piéce d'identité.\n\n")
+
+    print("\033[4mPiéce d'identité\033[0m\n")
+    nom_prenom = defs.demander_nom_prenom()
+    age = defs.demander_age()
+    nationalite = defs.demander_nationatlite()
+    raison_venue = defs.demander_raison_venue(nom_prenom)
+    print(f"{Fore.MAGENTA + Style.NORMAL}Garde{Style.RESET_ALL} : Tout me semble bon, vous pouvez passé.\n")
 
 
-# Créer un espace pour plus de visibilité
-print("\n")
+    # Appelle la fonction créer dans le module defs appeller pensee_une
+    pensee_une = defs.demander_pensee_une()
+
+
+        # Créer un espace pour plus de visibilité
+    print("\n")
+
